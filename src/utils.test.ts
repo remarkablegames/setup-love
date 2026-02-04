6a3892b7ccf4ec3dae174341c4bd8f87b94c962a
Mark <mark@remarkablemark.org>
2026-02-03 22:17:21 -0500

    test(jest): fix ESM tests
    https://jestjs.io/docs/ecmascript-modules

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,19 +1,25 @@
-import os from 'node:os';
+import type * as osType from 'node:os';
 
-import { getDownloadObject } from './utils';
+import { jest } from '@jest/globals';
 
-jest.mock('node:os');
+jest.unstable_mockModule('node:os', () => ({
+  platform: jest.fn(),
+}));
+
+const { getDownloadObject } = await import('./utils');
+const os = (await import('node:os')) as typeof osType;
+
 const mockedOs = jest.mocked(os);
 
 const platforms: NodeJS.Platform[] = ['darwin', 'linux', 'win32'];
 
 describe('getDownloadObject', () => {
-  describe.each(platforms)('when platform is %p', (os) => {
+  describe.each(platforms)('when platform is %p', (platform) => {
     const version = '11.4';
 
     beforeEach(() => {
       jest.resetAllMocks();
-      mockedOs.platform.mockReturnValueOnce(os);
+      mockedOs.platform.mockReturnValueOnce(platform);
     });
 
     it('gets download object', () => {
